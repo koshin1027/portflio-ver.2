f0e5fccff10e020130afe92d8b1ab880bad4dc70
KoshinWada <143990605+koshin1027@users.noreply.github.com>
2025-08-19 11:31:52 +0900

    Update Management.php
    各機能にコメントを追加

--- app/Livewire/Management.php
+++ app/Livewire/Management.php
@@ -46,20 +46,26 @@ class Management extends Component
     public $explanation = '';
     public $images = '';
 
+    //依存性注入
     public function boot(ManagementService $managementService)
     {
         $this->managementService = $managementService;
     }
 
+    //初期化
     public function mount()
     {
+        //DBからカテゴリーを全取得
         $this->categories = Category::all();
+
+        //取得したデータから最初のカテゴリーを$activeCategoryIdに格納
         $this->activeCategoryId = $this->categories->first()->id;
     }
 
+    //描画ごとの処理
     public function render()
     {
-        // ManagementServiceサービスに委譲
+        //managementServiceから「メニューのフィルタリング機能」を移譲
         $menus = $this->managementService->searchMenus(
             $this->activeCategoryId,
             $this->search,
@@ -67,6 +73,7 @@ class Management extends Component
             $this->sortPrice
         );
 
+        //フィルタリングされたメニューをlivewire.management.bladeに渡す
         return view('livewire.management',[
             'menus' => $menus,
         ]);
@@ -84,33 +91,38 @@ class Management extends Component
         $this->status = '';
         $this->amount = 0;
         $this->explanation = '';
-        // $this->images = '';
     }
 
-    //カテゴリータブで使用
+    //選択されたカテゴリーをアクティブカテゴリーにセット
     public function setActiveCategory($categoryId)
     {
         $this->activeCategoryId = $categoryId;
+
+        //カテゴリーが変わるたび検索欄を空白にリセット
         $this->search = '';
     }
 
-    // モーダル制御(注文追加)
+    // 注文追加用モーダルの制御処理
+
+    // オープン
     public function openAddModal()
     {
+        //オープン前に値をすべてリセット
         $this->resetForm();
+
         $this->isAddModalOpen = true;
     }
 
+    //クローズ
     public function closeAddModal()
     {
         $this->isAddModalOpen = false;
     }
 
-    // メニュー追加処理
+    // メニュー追加処理(追加ボタン押下時)
     public function submit()
     {
-
-        // ManagementServiceサービスに委譲
+        // managementServiceから「注文を追加する機能」を移譲
             $menus = $this->managementService->createMenu(
                 $this->name,
                 $this->price,
@@ -118,7 +130,6 @@ class Management extends Component
                 $this->status,
                 $this->amount,
                 $this->explanation,
-                // $images,
             );
 
         //フォームリセット
@@ -128,25 +139,33 @@ class Management extends Component
         $this->isAddModalOpen = false;
     }
 
-    //モーダル制御(編集)
+    // 注文編集用モーダルの制御処理
+
     public function openEditModal($id)
     {
+        //対象のIDを参照して$selectMenusに格納
         $this->selectMenus = Menu::find($id);
 
+        //フィールドを定義
         $fields = ['name', 'price', 'category_id', 'status', 'amount', 'explanation'];
 
+        //$selectMenusの情報を分割代入
         foreach ($fields as $field) {
             $this->$field = $this->selectMenus->$field;
         }
 
+        //対象IDを$editMenuIdに格納
         $this->editMenuId = $id;
+
+        //注文編集用モーダルをオープン
         $this->isEditModalOpen = true;
     }
 
     // アップデート処理
+
     public function update()
     {
-
+    //現在のプロパティを参照し、更新用のデータを定義
     $data = [
         'name' => $this->name,
         'price' => $this->price,
@@ -157,10 +176,10 @@ class Management extends Component
         // 'images' => $this->images,
     ];
 
-    // ManagementServiceサービスに委譲（$this->selectMenusは更新対象モデル）
+    //managementServiceから「注文を追加する機能」を移譲
     $menus = $this->managementService->updateMenu($this->selectMenus, $data);
 
-        //フォームリセット
+        //閉じる前にフォームリセット
         $this->resetForm();
 
         //モーダルを閉じる
